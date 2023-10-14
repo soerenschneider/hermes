cef13b88eb7f49aaacd8ad8ff3c4bca8eed8d1d9
Sören Schneider <56670304+soerenschneider@users.noreply.github.com>
2023-10-14 11:40:42 +0100

    test: add tests for http handler

---
+++ internal/events/http/http_test.go
@@ -0,0 +1,138 @@
+package http
+
+import (
+	"bytes"
+	"encoding/json"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+
+	"github.com/soerenschneider/hermes/pkg"
+)
+
+type DummyDispatcher struct {
+	not *pkg.NotificationRequest
+	src *string
+}
+
+func (d *DummyDispatcher) Accept(notification pkg.NotificationRequest, eventSource string) error {
+	d.not = &notification
+	d.src = &eventSource
+	return nil
+}
+
+func TestHttpServer_notifyHandler_wrongMethod(t *testing.T) {
+	req := httptest.NewRequest(http.MethodGet, "/notify", getValidReqest(t))
+	w := httptest.NewRecorder()
+
+	server := HttpServer{
+		dispatcher: &DummyDispatcher{},
+	}
+
+	server.notifyHandler(w, req)
+	res := w.Result()
+	expectedStatus := http.StatusMethodNotAllowed
+	defer res.Body.Close()
+	if expectedStatus != res.StatusCode {
+		t.Errorf("expected %d got %d", expectedStatus, res.StatusCode)
+	}
+}
+
+func TestHttpServer_notifyHandler_emptyBody(t *testing.T) {
+	req := httptest.NewRequest(http.MethodPost, "/notify", nil)
+	w := httptest.NewRecorder()
+
+	server := HttpServer{
+		dispatcher: &DummyDispatcher{},
+	}
+
+	server.notifyHandler(w, req)
+	res := w.Result()
+	expectedStatus := http.StatusBadRequest
+	defer res.Body.Close()
+	if expectedStatus != res.StatusCode {
+		t.Errorf("expected %d got %d", expectedStatus, res.StatusCode)
+	}
+}
+
+func TestHttpServer_notifyHandler_garbageData(t *testing.T) {
+	body := strings.NewReader("garbage")
+	req := httptest.NewRequest(http.MethodPost, "/notify", body)
+	w := httptest.NewRecorder()
+
+	server := HttpServer{
+		dispatcher: &DummyDispatcher{},
+	}
+
+	server.notifyHandler(w, req)
+	res := w.Result()
+	expectedStatus := http.StatusBadRequest
+	defer res.Body.Close()
+	if expectedStatus != res.StatusCode {
+		t.Errorf("expected %d got %d", expectedStatus, res.StatusCode)
+	}
+}
+
+func TestHttpServer_notifyHandler_ok_statuscode(t *testing.T) {
+	req := httptest.NewRequest(http.MethodPost, "/notify", getValidReqest(t))
+	w := httptest.NewRecorder()
+
+	server := HttpServer{
+		dispatcher: &DummyDispatcher{},
+	}
+
+	server.notifyHandler(w, req)
+	res := w.Result()
+	expectedStatus := http.StatusOK
+	defer res.Body.Close()
+	if expectedStatus != res.StatusCode {
+		t.Errorf("expected %d got %d", expectedStatus, res.StatusCode)
+	}
+}
+
+func TestHttpServer_notifyHandler_ok(t *testing.T) {
+	req := httptest.NewRequest(http.MethodPost, "/notify", getValidReqest(t))
+	w := httptest.NewRecorder()
+
+	dispatcher := &DummyDispatcher{}
+	server := HttpServer{
+		dispatcher: dispatcher,
+	}
+
+	server.notifyHandler(w, req)
+	res := w.Result()
+	expected := pkg.NotificationRequest{
+		ServiceId: serviceId,
+		Subject:   subject,
+		Message:   message,
+	}
+
+	defer res.Body.Close()
+
+	if expected != *dispatcher.not {
+		t.Errorf("expected %v got %v", expected, *dispatcher.not)
+	}
+}
+
+const (
+	serviceId = "example"
+	subject   = "example subject"
+	message   = "example message"
+)
+
+func getValidReqest(t *testing.T) io.Reader {
+	req := pkg.NotificationRequest{
+		ServiceId: serviceId,
+		Subject:   subject,
+		Message:   message,
+	}
+
+	data, err := json.Marshal(req)
+	if err != nil {
+		t.Error("can not marshal json, that's a bug", err)
+	}
+
+	return bytes.NewReader(data)
+}
