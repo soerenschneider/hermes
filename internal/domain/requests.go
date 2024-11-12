cd122a34516d0c46392e716ef2c4975b7aee917b
Sören Schneider <56670304+soerenschneider@users.noreply.github.com>
2024-11-12 10:03:13 +0100

    fix: require subject as according to openapi spec

--- internal/domain/requests.go
+++ internal/domain/requests.go
@@ -2,6 +2,6 @@ package domain
 
 type NotificationRequest struct {
 	ServiceId string `json:"service_id" validate:"required"`
-	Subject   string `json:"subject"`
+	Subject   string `json:"subject" validate:"required"`
 	Message   string `json:"message" validate:"required"`
 }
